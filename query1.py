b87d4d2e4dc919228d86b8f24721debed698abf3
yklhan <45218606+yklhan@users.noreply.github.com>
2018-12-06 01:10:43 -0800

    Add files via upload

---
+++ query1.py
@@ -0,0 +1,24 @@
+#the publication has most co-author
+import couchdb
+couchserver = couchdb.Server("http://couchdb:5984/")
+dbname = "dblp"
+db = couchdb.Database("http://localhost:5984/dblp/")
+paper = ""
+number = 0
+temp = {}
+for id in db:
+    if id != "_design/co-authors":
+        doc = db.get(id)
+        if doc['type'] == "paper" or doc['type'] == "book" or doc['type'] == "msthesis":
+            if doc.get('authors'):
+                if len(doc['authors']) >= number:
+                    number = len(doc['authors'])
+                    paper = doc['title']
+                    if number in temp.keys():
+                        temp[number].append(paper)
+                    else:
+                        temp[number] = [paper]
+resultN = max(temp.keys())
+resultP = temp[resultN]
+print(resultP, ": ",resultN)
+        
